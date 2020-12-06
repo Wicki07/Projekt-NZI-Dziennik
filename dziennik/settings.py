ab95950369c04297830fe0e0ccbdb525275c3ff3
Mateusz Wicki <71140843+Wicki07@users.noreply.github.com>
2020-12-06 18:13:03 +0100

    DZ-40 Weryfikacja plików

--- dziennik/settings.py
+++ dziennik/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_admin_logs',
+    'dziennik',
 ]
 
 MIDDLEWARE = [
@@ -56,7 +57,7 @@ ROOT_URLCONF = 'dziennik.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'dziennik', 'templates'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -129,9 +130,12 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),] 
+STATIC_ROOT = str(BASE_DIR.joinpath('static'))
+
+# Zmiana strony docelowej po zalogowaniu lub wylogowaniu
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
